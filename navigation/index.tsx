5499bc4518cf6c1e6e2c5b1117d90c566273f192
AnawaratneMANA <it19086408@my.sliit.lk>
2022-01-09 12:51:24 +0530

    Color added to the header.

--- navigation/index.tsx
+++ navigation/index.tsx
@@ -7,6 +7,7 @@ import NotFoundScreen from '../screens/NotFoundScreen';
 import { RootStackParamList } from '../types';
 import BottomTabNavigator from './BottomTabNavigator';
 import LinkingConfiguration from './LinkingConfiguration';
+import Colors from "../constants/Colors";
 
 // If you are not familiar with React Navigation, we recommend going through the
 // "Fundamentals" guide: https://reactnavigation.org/docs/getting-started
@@ -26,7 +27,16 @@ const Stack = createStackNavigator<RootStackParamList>();
 
 function RootNavigator() {
   return (
-    <Stack.Navigator screenOptions={{ headerShown: false }}>
+    <Stack.Navigator screenOptions={{ headerStyle: {
+            backgroundColor: Colors.light.tint,
+            shadowOpacity: 0,
+            elevation: 0,
+        },
+        headerTintColor: Colors.light.background,
+        headerTitleAlign: 'left',
+        headerTitleStyle: {
+            fontWeight: 'bold',
+        } }}>
       <Stack.Screen name="Root" component={BottomTabNavigator} />
       <Stack.Screen name="NotFound" component={NotFoundScreen} options={{ title: 'Oops!' }} />
     </Stack.Navigator>
